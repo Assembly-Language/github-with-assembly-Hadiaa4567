b136e0c03eff48b3be92f2a03de76cb70f32e661
Hadiaa4567 <hadiaanjum730@gmail.com>
2024-12-02 12:25:36 +0500

    last commit 1161

--- sum-even.asm
+++ sum-even.asm
@@ -1,18 +1,38 @@
-include irvine32.inc
+INCLUDE Irvine32.inc  
+
 .data
-
-; array and other variables declaration here
-
+  myArray word 2,4,9,13,6 
+  msgSum byte "Sum of Even Numbers : " ,0 
+  msgCount byte "Total Even Numbers: " ,0
+  sum dword 0
+  count dword 0 
 .code
-main proc
+main PROC
+  mov eax , 0
+  mov esi , OFFSET myArray
+  mov ecx,lengthof myArray
+EvenNumbers: 
+  mov ax , word ptr[esi]
+  test ax,1
+  jnz skipNegative
+  movsx eax,word ptr [esi]
+  add sum,eax 
+  add count , 1 
+skipNegative:
+  add esi,type myArray
+  loop EvenNumbers 
+  mov edx ,OFFSET msgSum
+  call writeString 
+  call crlf
+  mov eax , sum
+  call writeint 
+  call crlf
+  mov edx , OFFSET msgCount
+  call writeString
+  call crlf
+  mov eax , count
+  call writeint
+  exit
 
-
-;code here
-
-
-
-exit
-main endp
-
-
-end main
+main ENDP
+END main
