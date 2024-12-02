e2e1d4b1a905f92c8623edb73c3096789877ebab
Hadiaa4567 <hadiaanjum730@gmail.com>
2024-12-02 12:24:02 +0500

    second commit

--- sum-odd.asm
+++ sum-odd.asm
@@ -1,18 +1,39 @@
-include irvine32.inc
+INCLUDE Irvine32.inc  
+
 .data
-
-; array and other variables declaration here
-
+  myArray word 2,-3,5,6,-9 
+  msgSum byte "Sum of Negative Numbers : " ,0 
+  msgCount byte "Total Negative Numbers: " ,0
+  sum dword 0
+  count dword 0 
 .code
-main proc
+main PROC
+  mov eax , 0
+  mov esi , OFFSET myArray
+  mov ecx,lengthof myArray
+negativeNumbers: 
+  mov ax , word ptr[esi]
+  test ax,8000h
+  jnz skipNegative
+  movsx eax,word ptr [esi]
+  add sum,eax 
+  add count , 1 
+skipNegative:
+  add esi,type myArray
+  loop NegativeNumbers 
+  mov edx ,OFFSET msgSum
+  call writeString 
+  call crlf
+  mov eax , sum
+  call writeint 
+  call crlf
+  mov edx , OFFSET msgCount
+  call writeString
+  call crlf
+  mov eax , count
+  call writeint
+  exit
 
+main ENDP
+END main
 
-;code here
-
-
-
-exit
-main endp
-
-
-end main
